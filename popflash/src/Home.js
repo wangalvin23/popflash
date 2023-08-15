0ffc40de7aea012a8c8a04bdf3fe75d8c1383423
Alvin <wangalvin23@gmail.com>
2023-08-15 02:24:08 -0700

    testing commit again, again no major changes

--- popflash/src/Home.js
+++ popflash/src/Home.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
+//testing comment to make sure that everything is ok
+
 const Home = () => {
   const [flashes, setFlashes] = useState([]);
 
